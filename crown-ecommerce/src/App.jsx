a11dbb2c796c4fe7ea0793ca6cb26c4e303d12a0
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-03-17 14:18:24 -0300

    crown ecommerce skeleton

---
+++ crown-ecommerce/src/App.jsx
@@ -0,0 +1,12 @@
+import ProductCard from "./components/ProductCard";
+
+function App() {
+
+  return (
+    <div className="homepage">
+      <ProductCard productName="HATS" />
+    </div>
+  );
+}
+
+export default App;
