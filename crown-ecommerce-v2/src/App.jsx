4761352577337296b83011e5fd341cf0304da2d5
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-03-17 19:25:51 -0300

    crown ecommerce v2

---
+++ crown-ecommerce-v2/src/App.jsx
@@ -0,0 +1,13 @@
+import ProductCard from "./components/ProductCard";
+import Homepage from "./components/Homepage";
+
+function App() {
+
+  return (
+    <div className="container">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
