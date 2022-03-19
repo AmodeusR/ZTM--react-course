ed235116bbdef69220b29109c33e0c01c41e4559
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-03-19 20:37:44 -0300

    Update SignPage.jsx

--- crown-ecommerce-v2/src/pages/SignPage/SignPage.jsx
+++ crown-ecommerce-v2/src/pages/SignPage/SignPage.jsx
@@ -1,25 +1,20 @@
 import React from "react";
-import { signInWithGooglePopup as googleSignIn, userRegistration } from "/src/utils/firebase/firebase";
-import { Input, SignUpForm } from "../../components";
+import { signInWithGooglePopup as googleSignIn, googleUserAuth } from "/src/utils/firebase/firebase";
+import { SignUpForm, SignInForm } from "../../components";
 import "./sign-page.scss";
 
 const SignPage = () => {
   const signInWithGoogle = async () => {
     const { user } =  await googleSignIn();
-    const userDocRef = await userRegistration(user);
+    const userDocRef = await googleUserAuth(user);
+
+    console.log(userDocRef);
   }
 
   return (
     <div className="container">
-      {/* <button onClick={signInWithGoogle}>Sign in with Google</button> */}
-      <h1>Sign In Page</h1>
-
       <div className="sections">
-        <section className="sign-in">
-          <Input title="mail" type="email" required />
-          <Input title="password" type="password" required />
-        </section>
-        
+        <SignInForm title="Sing In" />
         <SignUpForm title="Sign up" />
       </div>
     </div>
