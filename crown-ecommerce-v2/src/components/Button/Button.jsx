c19aa6e751f02628eb7b707e2a286c51542135d3
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-03-20 00:46:17 -0300

    renamed button mainColor to color

--- crown-ecommerce-v2/src/components/Button/Button.jsx
+++ crown-ecommerce-v2/src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./button.scss";
 
-const Button = ({ title, className, mainColor, ...options }) => {
-  const style = { "--first-btn-color": mainColor }
+const Button = ({ title, className, color, ...options }) => {
+  const style = { "--first-btn-color": color }
   return (
     <button style={style} className={`button ${className || ""}`} {...options}>
       {title}
