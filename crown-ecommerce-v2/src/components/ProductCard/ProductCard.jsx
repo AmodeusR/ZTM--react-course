f8781baa8852ad2cc1774d88244874170cb75d70
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-03-27 22:20:13 -0300

    updated ProductCard Comp

--- crown-ecommerce-v2/src/components/ProductCard/ProductCard.jsx
+++ crown-ecommerce-v2/src/components/ProductCard/ProductCard.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { Button } from "../../components";
+import currencyFormatter from "../../utils/currencyFormatter";
+
 import "./product-card.scss";
 
 
 
-const ProductCard = ({ name, price, imgSrc }) => {
-
+const ProductCard = ({ product, handleItemAdditionToCart }) => {
+  const { name, price, imageUrl: imgSrc } = product;
+  const formattedPrice = currencyFormatter.format(price);
+  
   return (
     <div className="product-card">
       <img className="product-card__image" src={imgSrc} alt={`${name}`} />
       <div className="product-card__description">
         <span className="product-card__name">{name}</span>
-        <span className="product-card__price">{price}</span>
+        <span className="product-card__price">{formattedPrice}</span>
       </div>
-      <Button className="product-card__button inverted" title="Add to cart" />
+      <Button className="product-card__button inverted" title="Add to cart" onClick={() => handleItemAdditionToCart(product)} />
     </div>
   );
 };
