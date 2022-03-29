2a0e3b183ac8db297114e35ae6ec04e6427221f5
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-03-29 14:33:10 -0300

    Finished Checkout page component

--- crown-ecommerce-v2/src/pages/Checkout/Checkout.jsx
+++ crown-ecommerce-v2/src/pages/Checkout/Checkout.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import { useContext } from "react";
+import CartContext from "../../contexts/cart.context";
+import { CheckoutCard, Button } from "../../components";
 
 import "./checkout.scss";
+import currencyFormatter from "../../utils/currencyFormatter";
 
 const Checkout = () => {
+  const { cartItems } = useContext(CartContext);
+
+  const totalPrice = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
+  const FormattedtotalPrice = currencyFormatter.format(totalPrice);
+
   return (
-    <div>
-      <h1>Checkout Page</h1>
+    <div className="container checkout-body">
+      <h1>Checkout</h1>
+      <main className="payment-section">
+        <div className="payment-section__products">
+          {cartItems.map(({ id, name, imageUrl, price, quantity }) => (
+            <CheckoutCard
+              key={id}
+              id={id}
+              name={name}
+              imageUrl={imageUrl}
+              price={price}
+              quantity={quantity}
+            />
+          ))}{" "}
+        </div>
+        <div className="payment-section__confirmation">
+          <Button title="Confirm Payment" className="payment-section__button" />
+          <span className="payment-section__total-price">
+            {FormattedtotalPrice}
+          </span>
+        </div>
+      </main>
     </div>
   );
 };
