01e41a7f2d7891ec132648ea15dd3867336f00dd
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-03-30 09:40:57 -0300

    deleted unnecessary div

--- crown-ecommerce-v2/src/components/Navigation/Navigation.jsx
+++ crown-ecommerce-v2/src/components/Navigation/Navigation.jsx
@@ -15,47 +15,55 @@ const Navigation = () => {
 
   const handleSignOut = async () => {
     await userSignOut();
-  }
+  };
 
   return (
     <>
       <header className="header">
-        <div className="navbar-crossline">
-          <nav className="navbar container">
-            <Link to="/" >
-              <Logo className="navbar__logo" alt="Crown logo. Click to go to homepage." />
-            </Link>
-            <ul className="navbar__list">
-              <li className="navbar__list-item">
-                <Link className="navbar__list-link" to="/">
-                  Home
+        <nav className="navbar container">
+          <Link to="/">
+            <Logo
+              className="navbar__logo"
+              alt="Crown logo. Click to go to homepage."
+            />
+          </Link>
+          <ul className="navbar__list">
+            <li className="navbar__list-item">
+              <Link className="navbar__list-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="navbar__list-item">
+              <Link className="navbar__list-link" to="/shop">
+                Shop
+              </Link>
+            </li>
+            <li className="navbar__list-item">
+              <Link className="navbar__list-link" to="/about">
+                About
+              </Link>
+            </li>
+            <li className="navbar__list-item">
+              {user ? (
+                <span className="navbar__list-link" onClick={handleSignOut}>
+                  Sign Out
+                </span>
+              ) : (
+                <Link className="navbar__list-link" to="/auth">
+                  Sign In
                 </Link>
-              </li>
-              <li className="navbar__list-item">
-                <Link className="navbar__list-link" to="/shop">
-                  Shop
-                </Link>
-              </li>
-              <li className="navbar__list-item">
-                <Link className="navbar__list-link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li className="navbar__list-item">
-                { user ?
-                  <span className="navbar__list-link" onClick={handleSignOut}>Sign Out</span> :
-                  <Link className="navbar__list-link" to="/auth">
-                    Sign In
-                  </Link>
-                }
-              </li>
-            </ul>
-                <div tabIndex="23" className="cart-container" onBlur={() => setIsCartOpen(false)}>
-                  <CartIcon />
-                  <CartDropdown />
-                </div>
-          </nav>
-        </div>
+              )}
+            </li>
+          </ul>
+          <div
+            tabIndex="23"
+            className="cart-container"
+            onBlur={() => setIsCartOpen(false)}
+          >
+            <CartIcon />
+            <CartDropdown />
+          </div>
+        </nav>
       </header>
       <Outlet />
     </>
