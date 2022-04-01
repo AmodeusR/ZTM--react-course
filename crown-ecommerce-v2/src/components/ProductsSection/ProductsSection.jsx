93a2c4a199b2e9244875d21a655fb80e5beecabc
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-01 00:06:39 -0300

    updated ProductsSection

--- crown-ecommerce-v2/src/components/ProductsSection/ProductsSection.jsx
+++ crown-ecommerce-v2/src/components/ProductsSection/ProductsSection.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { ProductCard } from "/src/components";
 
 import "./products-section.scss";
@@ -6,14 +7,21 @@ import CartContext from "../../contexts/cart.context";
 
 
 
-const ProductsSection = ({ title, products }) => {
+const ProductsSection = ({ title, products, preview = false }) => {
   const { addItemToCart } = useContext(CartContext);
+
+  const productsToMap = preview ? products.filter((_, i) => i < 4) : products;  
 
   return (
     <section className="products-section">
-      <h2 className="products-section__title">{title}</h2>
+      <div className="products-section__header-container">
+        <h2 className="products-section__title">{title}</h2>
+        {!preview && 
+          <Link className="products-section__return" to="/shop">Go back</Link>
+        }
+      </div>
       <div className="products-section__list">
-        {products.map((product) => (
+        {productsToMap?.map((product) => (
           <ProductCard
             product={product}
             key={product.id}
@@ -21,6 +29,9 @@ const ProductsSection = ({ title, products }) => {
           />
         ))}
       </div>
+      {preview &&
+        <Link to={`${title.toLowerCase()}`} className="products-section__expand">See more</Link>
+      }
     </section>
   );
 };
