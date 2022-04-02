c449e072c8acc1a8781f50ed86c7e8a1da30ff3e
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-04-02 11:44:50 -0300

    Created isFetching variable
    Created isFetching variable to keep track of whether or not the content has been loaded ot not

--- crown-ecommerce-v2/src/contexts/category.context.jsx
+++ crown-ecommerce-v2/src/contexts/category.context.jsx
@@ -1,25 +1,28 @@
 import { createContext, useState, useEffect } from "react";
-import SHOP_DATA from "/src/shop-data.js";
 
 import { fetchData } from "../utils/firebase/firebase";
 
 const CategoryContext = createContext({
   categories: [],
   setCategories: () => null,
+  isFetching: true
 });
 
 export const CategoryProvider = ({ children }) => {
   const [categories, setCategories] = useState({});
+  const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const categoryMap = await fetchData();
-
-        setCategories(categoryMap);
-      } catch (error) {
-        console.warn(error);
-      }
+          const categoryMap = await fetchData();
+          setCategories(categoryMap);
+          
+        } catch (error) {
+          console.warn(error);
+        } finally {
+          setIsFetching(false);
+        }
     };
     
     fetchCategories();
@@ -31,6 +34,7 @@ export const CategoryProvider = ({ children }) => {
       value={{
         categories,
         setCategories,
+        isFetching
       }}
     >
       {children}
